8464d960c8d414237b151714773c63f4a3683871
hassanr-coder <hassanr@students.trinitydc.edu>
2026-03-17 17:35:10 -0400

    create

---
+++ dictionary_retrieval.py
@@ -0,0 +1,20 @@
+'''
+Rayaan Hassan
+CMSC 111
+Spring 2026
+Week 7 Assignment 2
+'''
+# Define the dictionary
+employee = {
+    'name': 'Rayaan Hassan',
+    'age': 23,
+    'department': 'Information technology',
+    'salary': 50000
+}
+
+# Retrieve the values using the keys
+dept = employee['department']
+sal = employee['salary']
+
+# Print the retrieved values
+print(f"department: {dept} Salary: {sal}")
