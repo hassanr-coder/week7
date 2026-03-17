3468b867175b8ddb5f14d3d70d5a9965831bfa76
hassanr-coder <hassanr@students.trinitydc.edu>
2026-03-17 17:29:10 -0400

    create

---
+++ student_dictionary.py
@@ -0,0 +1,15 @@
+'''
+Rayaan Hassan
+CMSC 111
+Spring 2026
+Week 7 Assignment 1
+'''
+# Create a dictionary to store student information
+student= {
+    'name': 'Rayaan Hassan',
+    'age': 23,
+    'major': 'Information Technology',
+}
+
+# Print the student's dictionary
+print(student)
