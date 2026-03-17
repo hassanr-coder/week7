caf27a05a4cb338cef681896bee5974293c0b1e8
hassanr-coder <hassanr@students.trinitydc.edu>
2026-03-17 18:19:22 -0400

    create

---
+++ merge_dictionaries.py
@@ -0,0 +1,15 @@
+'''
+Rayaan Hassan
+CMSC 111
+Spring 2026
+Week 7 Assignment 4
+'''
+# Define the two dictionaries
+dict1 = {"a": 1, "b": 2}
+dict2 = {"b": 3, "c": 4}
+
+# Merge the two dictionaries into a new dictionary
+merged_dict = dict1  | dict2
+
+# Print the merged dictionary
+print(merged_dict)  
